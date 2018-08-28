817c24c3e8cd8356d668b849b9ab6792a8e9570e
alejandrog5 <a.godoylee@gmail.com>
2018-08-27 21:22:16 -0600

    Lab 1.5

--- Lab1.5/search.sh
+++ Lab1.5/search.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 direc='/home/alejandro/Desktop/src/*'
-juanito='/home/alejandro/Desktop/os-labs/Lab1.5/linux_source_structure.txt'
+output='/home/alejandro/Desktop/os-labs/Lab1.5/linux_source_structure.txt'
 
 for dir in $direc; do
     if [ -d "$dir" ]; then
-        echo 'Directory: '$dir >> $juanito
-        echo 'Files with .c' >> $juanito
-        find $dir -name '*.c'  >> $juanito
-        echo 'Files with .h'  >> $juanito
-        find $dir -name '*.h' >> $juanito
+        echo 'Directory: '$dir >> $output
+        echo 'Files with .c' >> $output
+        find $dir -name '*.c'  >> $output
+        echo 'Files with .h'  >> $output
+        find $dir -name '*.h' >> $output
     fi
 done
 
