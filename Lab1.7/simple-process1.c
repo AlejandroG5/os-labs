61465f4c516aede20472a7de587a04ecefa5658f
alejandrog5 <a.godoylee@gmail.com>
2018-08-31 15:21:06 -0600

    SJF-Scehduler

---
+++ Lab1.7/simple-process1.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+
+
+enum state {IDLE,READY,WAIT,RUN};
+
+typedef struct{
+	int burst_time;
+	int id;
+	int priority;
+	int waiting_time;
+	enum state state;
+}process;
+
+
+void createprocess(int,int,int);
+void insertionburst();
+void activateburst();
+
+process pg[10];
+process psjf[10];
+int size=sizeof(pg)/sizeof(process);
+int time=0;
+
+int main(void){
+	int m,a,b;
+	for(m=0;m<size;m++){
+		createprocess(m,100-m*6,1);
+	}
+	createprocess(3,2,1);
+	createprocess(5,10,1);
+	createprocess(7,99,1);
+	createprocess(1,56,1);
+	insertionburst();
+	for(a=1;a<size;a++){
+		time+=pg[a-1].burst_time;
+		pg[a].waiting_time=time;
+	}
+	for(b=0;b<size;b++){
+		activateburst(b);
+	}
+	printf("Average wait time: %d\n",(time/size));
+	return 0;
+	
+}
+
+void createprocess(int id, int burst, int priority){
+	pg[id].id=id;
+	pg[id].burst_time=burst;
+	pg[id].priority=priority;
+	pg[id].state=READY;
+}
+
+void activateburst(int b){
+	pg[b].state=RUN;
+	int i;
+	//........ FUNCTIONALITIES OF EACH PROCESS
+	printf("Running task no. %d\n", b);
+	for(i=0;i<size;i++){
+		printf(" ID: %d STATE: %d burst time: %d sec waiting time: %d sec\n",pg[i].id , pg[i].state , pg[i].burst_time, pg[i].waiting_time);
+	}
+	pg[b].state=IDLE;
+	printf("Finished task no. %d\n", b);
+}
+void insertionburst(){
+	int i, j;
+	process key;
+	for (i=1;i<size;i++){
+		key = pg[i];
+        	j = i-1;
+       		while (j >= 0 && pg[j].burst_time > key.burst_time){
+          		pg[j+1] = pg[j];
+      	        	j = j-1;
+        	}
+        pg[j+1] = key;
+	}
+}
